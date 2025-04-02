67c2a6cba6a6ed8d0690c97e56c76d232b02a681
SaladinX1 <hopefulhorizon01@gmail.com>
2025-04-02 10:25:29 +0200

    Définition des types pour olympic.service.ts

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -9,13 +9,13 @@ import { Country } from '../models/Olympic'
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<Country | null>(null);
+  private olympics$ = new BehaviorSubject<Country[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<Country>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
+    return this.http.get<Country[]>(this.olympicUrl).pipe(
+      tap((countries) => this.olympics$.next(countries)),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
@@ -25,6 +25,18 @@ export class OlympicService {
       })
     );
   }
+
+  // onSelect(data): void {
+  //   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  // }
+
+  // onActivate(data): void {
+  //   console.log('Activate', JSON.parse(JSON.stringify(data)));
+  // }
+
+  // onDeactivate(data): void {
+  //   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  // }
 
   getOlympics() {
     return this.olympics$.asObservable();
