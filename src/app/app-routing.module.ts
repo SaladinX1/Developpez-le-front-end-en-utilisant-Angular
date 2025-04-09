00e8f4d2ef49089dbc31b775bbdfd759a06f2ed2
SaladinX1 <hopefulhorizon01@gmail.com>
2025-04-09 23:15:00 +0200

    app-routing-module.ts - Définition du path de la page country_detail + component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DetailCountryComponent } from './pages/detail-country/detail-country.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'country_detail/:id',
+    component: DetailCountryComponent,
   },
   {
     path: '**', // wildcard
